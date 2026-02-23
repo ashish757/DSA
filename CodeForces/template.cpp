a4dc24ed258bc2fbe41032b753fa83a85aa08626
Ashish Raj Singh <ashishrajsingh75@gmail.com>
2026-02-23 16:49:14 +0530

    template

---
+++ CodeForces/template.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- Macros & Type Aliases ---
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+// --- Debugging (Only active on local machine) ---
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+// --- Core Logic ---
+void solve() {
+    // Your code here
+}
+
+int main() {
+    fast_io;
+
+    int t = 1;
+    cin >> t; // Comment this out if the problem has only one test case
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
